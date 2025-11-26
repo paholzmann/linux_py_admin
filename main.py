b470eea9aa2bce5b5365084b3e7f8699b78171cf
Holzmann <patrickholzmann4@gmail.com>
2025-11-26 09:49:45 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,18 @@
+from src.data import GenerateUserData
+
+class Main:
+    def __init__(self):
+        """
+        Docstring for __init__
+        
+        :param self: Description
+        """
+        self.generate_user_data = GenerateUserData()
+
+    def create_user_workflow(self):
+        """
+        Docstring for create_user_workflow
+        
+        :param self: Description
+        """
+        
